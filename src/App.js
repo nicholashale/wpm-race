5645094ceb4bda65c4c383c586caea5d26304ab6
Nicholas Weinberg <nickweinberg@fastmail.com>
2022-06-15 21:43:43 -0700

    textbox checking done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import randomWords from "random-words";
+
+const wordsMax = 250;
+const timeLimit = 60;
 
 function App() {
+  const [text, setText] = useState([]);
+  const [position, setPosition] = useState(0);
+  const [typedText, setTypedText] = useState("");
+
+  useEffect(() => {
+    setText(randomWords(wordsMax));
+  }, []);
+
+  function handleKeystroke(event) {
+    const newTypedText = event.target.value;
+    if (newTypedText === text[position]) {
+      setTypedText("");
+      setPosition(position + 1);
+    } else {
+      setTypedText(newTypedText);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {/* Text: {text.join(", ")} <br /> */}
+        Current word: {text[position]}
+      </div>
+      <input type="text" onChange={handleKeystroke} value={typedText} />
     </div>
   );
 }
