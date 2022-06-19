79a6e4bdaf966cff5bb5bc5b781548eb212b0b2d
Nicholas Weinberg <nickweinberg@fastmail.com>
2022-06-18 18:01:19 -0700

    wip

---
+++ backend/app/controllers/users_controller.rb
@@ -0,0 +1,57 @@
+class UsersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+    def index
+        users = User.all
+        render json: users
+    end
+
+    def show
+        user = find_user
+        if user
+            render json: user
+        else
+            render json: { error: "Not found" }, status: :not_found
+        end
+    end
+
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            render json: user, status: :created
+        else
+            render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end 
+    end
+
+    def update
+        user = find_user
+        user.update!(user_params)
+        render json: user
+    end
+
+    def destroy
+        user = find_user
+        user.destroy
+        head :no_content
+    end
+
+    private
+
+    def find_user
+        User.find(params[:id])
+    end
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation)
+    end
+
+    def render_not_found_response
+        render json: {error: "User not found"}, status: :not_found
+    end
+    
+    def render_unprocessable_entity_response
+        render json: {errors: "Invalid User entity"}, status: :unprocessable_entity
+    end
+end
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,7 +1,19 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+puts "🗑 Clearing database"
+
+User.destroy_all
+
+puts "✅ Done cleaning!"
+
+puts "🌱 Seeding database"
+
+joe = User.create(username: "zamboni", password_digest: BCrypt::Password.create("letmein"))
+chandy = User.create(username: "chandizzy123", password_digest: BCrypt::Password.create("hunter2"))
+nick = User.create(username: "harper", password_digest: BCrypt::Password.create("asdfasdf"))
+phil = User.create(username: "rothberry", password_digest: BCrypt::Password.create("beets"))
+
+
+puts "✅ Done seeding!"
