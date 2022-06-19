2f86fee2efea445164fd105b55babbf285c5f8e9
Nicholas Weinberg <nickweinberg@fastmail.com>
2022-06-18 17:02:00 -0700

    fixed react error unique key id in spans

--- src/AssText.js
+++ src/AssText.js
@@ -5,17 +5,21 @@ import { useAssContext } from "./assState";
 
 export default function AssText() {
   const [state, dispatch] = useAssContext();
+
   return (
     <div className={styles.text}>
       {state.text.map((word, wordIndex) => {
         return (
           <span
+            key={wordIndex}
             className={classNames(styles.word, {
               [styles.correct]: state.position > wordIndex,
             })}
           >
-            {word.split("").map((letter) => (
-              <span className={styles.letter}>{letter}</span>
+            {word.split("").map((letter, letterIndex) => (
+              <span key={letterIndex} className={styles.letter}>
+                {letter}
+              </span>
             ))}
           </span>
         );
