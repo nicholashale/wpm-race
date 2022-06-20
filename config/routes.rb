f942304d4ce2fb982ea8839e9d0aa26c8a85517a
Nicholas Weinberg <nickweinberg@fastmail.com>
2022-06-19 18:47:30 -0700

    prepped files and structure for deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  resources :reports
-  resources :races
-  resources :users
+  namespace :api do
+    resources :reports
+    resources :races
+    resources :users
   
-  post "/login", to: "sessions#create"
-  post "/signup", to: "users#create"
-  delete "/logout", to: "sessions#destroy"
+    post "/login", to: "sessions#create"
+    post "/signup", to: "users#create"
+    delete "/logout", to: "sessions#destroy"
+  end
+  
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
